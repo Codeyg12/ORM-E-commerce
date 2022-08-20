182c296b9519bfe9e551f30bb1d4b4c68416990e
Codeyg12 <codey.gallup@gmail.com>
2022-08-19 20:01:40 -0600

    added all needed routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -72,7 +72,7 @@ router.delete("/:id", async (req, res) => {
       res.status(404).json({ message: "No tag found with that id" });
       return;
     }
-    res.status(200).json(tagData);
+    res.status(200).json({ message: `${tagData} tag deleted`});
   } catch (err) {
     res.status(500).json(err);
   }
